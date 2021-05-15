2e58e8eef6b0fbbaf0bfd671223b7a07156d9686
usUpal <udyan.upal@northsouth.edu>
2021-05-15 09:13:36 +0600

    eventemitter 1.0

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-const fs  = require('fs')
-const { reject, result } = require('lodash')
-const { resolve } = require('path')
+const EventEmitter = require('events')
 
-const getText = (path) => {
-    return new Promise((resolve, reject) =>{
-        fs.readFile(path, 'utf8', (err,data)=> {
-            if(err){
-                reject(err)
-            }
-            else{
-                resolve(data)
-            }
-        })
-    })
-}
-getText('./content/doc.txt')
-  .then(result => console.log(result))
-  .catch(err => console.log(err))
+const customEmitter = new EventEmitter()
