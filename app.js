ca0fb7020e3adcd5e337b64a2d84292d6911a984
usUpal <udyan.upal@northsouth.edu>
2021-05-15 06:02:58 +0600

    async 1.0

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const http = require('http')
 const server = http.createServer((req, res) => {
     if(req.url == '/'){
         res.end('home')
-    }if (req.url == '/about') {
+    }
+    if(req.url == '/about'){
         res.end('about')
-    }else{
-        res.end('page broken')
+    }
+    else{
+        res.end('404 error')
     }
 })
 
-server.listen(5000)
+server.listen(5000,() =>{
+    console.log('server listening in port 5000');
+})
